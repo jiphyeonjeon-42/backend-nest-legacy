37f277c73aa43f49debacae4dd87f688e99a843a
DiceMono <hyekim@student.42seoul.kr>
2021-08-28 17:17:47 +0900

    FEATURE: csv to database

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BookInfo } from './entities/bookInfo.entity';
+import { Book } from './entities/book.entity';
+
 
 @Module({
   controllers: [BooksController],
   providers: [BooksService],
+  imports: [TypeOrmModule.forFeature([BookInfo, Book])],
+  exports: [TypeOrmModule]
 })
 export class BooksModule {}
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,15 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { BookInfo } from './entities/bookInfo.entity';
+import { Book } from './entities/book.entity';
+import {
+  paginate,
+  Pagination,
+  IPaginationOptions,
+} from 'nestjs-typeorm-paginate';
+
 
 @Injectable()
 export class BooksService {
+  constructor(
+    @InjectRepository(BookInfo)
+    private bookInfosRepository: Repository<BookInfo>,
+    @InjectRepository(Book)
+    private booksRepository: Repository<Book>,
+  ) {}
+
   create(createBookDto: CreateBookDto) {
     return 'This action adds a new book';
-  }
+  } 
 
-  findAll() {
-    return `This action returns all books`;
+  async search(options: IPaginationOptions) {
+    return paginate(this.bookInfosRepository, options);
   }
 
   findOne(id: number) {
