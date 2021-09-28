158936ffb6efaa2506655195bf2c182c5ec4c093
DiceMono <hyekim@student.42seoul.kr>
2021-09-28 17:29:15 +0900

    ENV: update enableCors

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,10 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   await app.listen(port);
 }
 bootstrap();
