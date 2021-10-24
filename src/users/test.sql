6d05c4673572dad1f0d07216d747f02a52f1e855
DiceMono <hyekim@student.42seoul.kr>
2021-10-24 20:59:54 +0900

    FIX: rm unnecessary file

--- src/users/test.sql
+++
@@ -1,4 +0,0 @@
-SELECT 
-`user`.`id` AS `user_id`, `user`.`login` AS `user_login`, `reservations`.`id` AS `reservations_id`, `reservations`.`userId` AS `reservations_userId`, `reservations`.`bookId` AS `reservations_bookId`, 
-(SELECT COUNT(case when `reservations`.`canceledAt` IS NOT NULL or (`reservations`.`endAt` is NOT NULL) then 1 end) FROM `reservation` `reservations`) AS `xxx` FROM `user` `user` 
-LEFT JOIN `reservation` `reservations` ON `reservations`.`userId`=`user`.`id
