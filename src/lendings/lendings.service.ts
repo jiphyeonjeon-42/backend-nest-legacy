25a6dc0bb3f5a19544dc8aaa767d85f5c72f7c14
chanyoung <cksck8576@naver.com>
2021-10-31 11:40:22 +0900

    FEATURE : 새로운 책을 대출할 때 나오는 오류를 수정하였습니다

--- src/lendings/lendings.service.ts
+++ src/lendings/lendings.service.ts
@@ -81,7 +81,9 @@ export class LendingsService {
     if (reservationData != undefined && reservationData.user.id != dto.userId)
       throw new BadRequestException('예약된 책입니다.');
     const lendingData = await this.getLending(dto.bookId);
-    if (!lendingData.returning)
+
+    if (lendingData == undefined) {
+    } else if (lendingData != undefined && !lendingData.returning)
       throw new BadRequestException('대출된 책입니다.');
     try {
       await this.lendingsRepository.insert({
@@ -173,8 +175,6 @@ export class LendingsService {
       .where('book.id=:bookId', { bookId: bookId })
       .orderBy('lending.createdAt', 'DESC')
       .getOne();
-
-    if (lendingData == undefined) return undefined;
     return lendingData;
   }
 
