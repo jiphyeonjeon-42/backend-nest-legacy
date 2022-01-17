de9d0e1826ba9756313b4fa1e8741ace9760bfde
HyeongSeon <hyekim@student.42seoul.kr>
2022-01-17 23:26:38 +0900

    [UPDATE] update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,2 @@
 ## run docker containers
-docker-compose -f ~/backend/docker-compose.yml up --build -d
-
-## set basic data to database
-docker-compose -f ~/backend/docker-compose.yml exec backend npm run seed
-docker-compose -f ~/backend/docker-compose.yml exec backend npm run bulk
+sudo docker-compose -f ~/backend/docker-compose.yml up --build -d
